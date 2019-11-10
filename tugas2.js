f05ab190692376db93a5c68d4386382554b8cdb7
Muh Noer Fathir Nasrullah <muhnoerfathir@gmail.com>
2019-11-10 20:59:13 +0700

    Tugas 2 JS
    Done ya

---
+++ tugas2.js
@@ -0,0 +1,18 @@
+var number = 9;
+var string = "makan banyak";
+var boolean = true;
+var nullya = null;
+var undifined;
+var array = [5,1,3,6,7,8];
+
+if(number<8)
+boolean = true;
+else
+boolean = false;
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(nullya);
+console.log(undifined);
+console.log(array[5]);
